298ca6a86fd28fe744b57b97c831981d1345da7b
Noxius <dg2c4u@gmail.com>
2026-03-10 18:50:57 -0700

    formatting

--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,11 @@
-import crypto from 'crypto';
-import fs from 'fs';
+import crypto from "crypto";
+import fs from "fs";
 
-export const createMD5 = (path: string) => new Promise<string>((resolve, reject) => {
-    const hash = crypto.createHash('md5')
-    const rs = fs.createReadStream(path)
-    rs.on('error', reject)
-    rs.on('data', chunk => hash.update(chunk))
-    rs.on('end', () => resolve(hash.digest('hex')))
-});
+export const createMD5 = (path: string) =>
+    new Promise<string>((resolve, reject) => {
+        const hash = crypto.createHash("md5");
+        const rs = fs.createReadStream(path);
+        rs.on("error", reject);
+        rs.on("data", (chunk) => hash.update(chunk));
+        rs.on("end", () => resolve(hash.digest("hex")));
+    });
